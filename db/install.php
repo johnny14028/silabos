81f235b0f47f8eed7a497423a8149db5d7dac054
ehuamanip <ehuamanip@pucp.pe>
2018-02-15 22:25:33 -0500

    agregar tabla en el install

--- db/install.php
+++ db/install.php
@@ -1,7 +1,23 @@
 <?php
 
-function xmldb_local_silabos_install()
-{
-
-    return true;    
+function xmldb_local_silabos_install() {
+    global $DB;
+    //listaremos todos los cursos y procederemos a crear los registros en el inidate
+    $sql_courses = "SELECT * FROM {course} WHERE category > :category";
+    $courses = $DB->get_records_sql($sql_courses, ['category' => 0]);
+    if (is_array($courses) && count($courses) > 0) {
+        foreach ($courses as $index => $objCourse) {
+            $objBeanSilabos = new stdClass();
+            $objBeanSilabos->int_courseid = $objCourse->id;
+            $objBeanSilabos->chr_course_shortname = $objCourse->shortname;
+            $objBeanSilabos->chr_course_fullname = $objCourse->fullname;
+            $objBeanSilabos->int_groupid = 0;
+            $objBeanSilabos->chr_group_name = '';
+            $objBeanSilabos->is_active = 0;
+            $objBeanSilabos->is_deleted = 0;
+            $objBeanSilabos->date_timecreated = time();
+            $objBeanSilabos->date_timemodified = time();
+            $DB->insert_record('local_silabos', $objBeanSilabos);
+        }
+    }
 }
